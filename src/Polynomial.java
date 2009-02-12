17ed6e0e6008d99015425513f91b40136f42f252
nem7760 <nem7760@rit.edu>
2009-02-12 21:45:50 +0000

    Added a Polynomial class so we can find the chromatic polynomial and then use that to find the solution more efficiently.

---
+++ src/Polynomial.java
@@ -0,0 +1,67 @@
+/*
+ * Implements a mutable representation of a polynomial
+ * of arbitrary degree. Coefficients can be modified,
+ * other polynomials can be added or subtracted, the 
+ * polynomial can be evaluated at different values of 
+ * X, etc.
+ */
+
+public class Polynomial {
+	int[] coefficients;
+
+	public Polynomial( int degree ) {
+		// allocate coefficients for up to 
+		// x^(degree) all the way down to 
+		// the contstant term at the end
+		coefficients = new int[ degree + 1 ];
+		
+		for( int i = 0; i <= degree; i++ ) {
+			coefficients[ i ] = 0;
+		}
+	}
+
+	/*
+	 * Add val to the coefficient specified.
+	 */
+	public void addToCoefficient( int val, int coef ) {
+		coefficients[ coef ] += val;
+	}
+
+	/*
+	 * Subtract val from the coefficient specified.
+	 */
+	public void subFromCoefficient( int val, int coef ) {
+		coefficients[ coef ] -= val;
+	}
+
+	/*
+	 * Add an entire polynomial to this one.
+	 */
+	public void addPolynomial( Polynomial val ) {
+		for( int i = 0; i < coefficients.length; i++ ) {
+			this.coefficients[ i ] += val.coefficients[ i ];
+		}
+	}
+
+	/*
+	 * Subtract an entire polynomial from this one.
+	 */
+	public void subPolynomial( Polynomial val ) {
+		for( int i = 0; i < coefficients.length; i++ ) {
+			this.coefficients[ i ] -= val.coefficients[ i ];
+		}
+	}
+
+	/*
+	 * Evaluates this polynomial at the desired value of X
+	 */
+	public long evalPolynomial( int xVal ) {
+		long result = coefficients[ 0 ];
+
+		for( int i = 1; i < coefficients.length; i++ ) {
+			result += coefficients[ i ]*Math.pow( xVal, i );
+		}
+
+		return result;
+	}
+}
